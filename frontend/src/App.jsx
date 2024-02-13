326ccf8d54fca353e5c481fcf773175259ca145d
Andrew Okamoto <andy.okamoto@gmail.com>
2024-02-12 22:31:24 -0700

    added routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,30 @@
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
-import './App.css'
+import { QueryClient, QueryClientProvider } from "react-query";
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import "./App.css";
 
 const queryClient = new QueryClient();
 
 function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<h1>placeholder</h1>} />
-        </Routes>
-      </BrowserRouter>
-    </QueryClientProvider>
-  );
+    return (
+        <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+                <Routes>
+                    <Route
+                        path="/"
+                        element={<h1>placeholder</h1>}
+                    />
+                    <Route
+                        path="/chats"
+                        element={<h1>/chats</h1>}
+                    />
+                    <Route
+                        path="/chats/:chatId"
+                        element={<h1>/chats/chatId</h1>}
+                    />
+                </Routes>
+            </BrowserRouter>
+        </QueryClientProvider>
+    );
 }
 
-export default App
+export default App;
