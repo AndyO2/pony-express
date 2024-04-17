6c46a580d6ce6b1cee6f6f0dbd3812e474f7126f
andyokamoto <andy.okamoto@gmail.com>
2024-04-16 18:27:19 -0600

    copy over api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,4 +1,11 @@
 const api = (token) => {
+  // generally, you access environment using `process.env`
+  // in Vite, you access via `import.meta.env`
+  //    - by default only gives access to env variables that
+  //      are prefixed with `VITE_`
+  // we can access env variables defined outside (eg export VITE_VAR=hi)
+  // or defined in a .env file
+
   const baseUrl = import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000";
 
   const headers = {
@@ -9,17 +16,18 @@ const api = (token) => {
     headers["Authorization"] = "Bearer " + token;
   }
 
-  const get = (url) =>
-    fetch(baseUrl + url, { method: "GET", headers, mode: "no-cors" });
+  // get("/users/me") ~> finish the same way we would with fetch
+  const get = (url) => fetch(baseUrl + url, { method: "GET", headers });
 
+  // post("/auth/registration", { username, email, password })
   const post = (url, body) =>
     fetch(baseUrl + url, {
       method: "POST",
       body: JSON.stringify(body),
       headers,
-      mode: "no-cors",
     });
 
+  // postForm("/auth/token", { username, password })
   const postForm = (url, body) =>
     fetch(baseUrl + url, {
       method: "POST",
@@ -28,7 +36,6 @@ const api = (token) => {
         ...headers,
         "Content-Type": "application/x-www-form-urlencoded",
       },
-      mode: "no-cors",
     });
 
   return { get, post, postForm };
